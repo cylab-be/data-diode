ceaa4b649a9479d50d0e857aad3afc6dfc2ec14d
Renaud Gaspard <rgaspard@ulb.ac.be>
2017-11-02 11:05:36 +0100

    Added laravel-test directory containing a test laravel site with login

---
+++ laravel-test/resources/views/config.blade.php
@@ -0,0 +1,26 @@
+@extends("layouts.app")
+
+@section("content")
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Interfaces</div>
+				<div class="panel-body">
+					Interface configuration
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Port Forwarding</div>
+				<div class="panel-body">
+					Port forwarding configuration
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
