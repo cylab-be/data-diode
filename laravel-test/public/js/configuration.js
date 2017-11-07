e61908604b470c89829a34dd5492319de9911ba4
Renaud Gaspard <rgaspard@ulb.ac.be>
2017-11-07 20:18:41 +0100

    Added row edition client side

--- laravel-test/public/js/configuration.js
+++ laravel-test/public/js/configuration.js
@@ -3,12 +3,18 @@ $(() => {
         layout:"fitColumns",
         columns:[
             {title:"ID", field:"id", visible: false},
-            {title:"Input Port", field:"input_port"},
-            {title:"Ouput Port", field:"output_port"}
+            {title:"Input Port", field:"input_port", editor: "input", validator: ["unique", "required", "integer", "min:0", "max:65535"]},
+            {title:"Ouput Port", field:"output_port", editor: "input", validator: ["unique", "required", "integer", "min:0", "max:65535"]},
+            {formatter: "buttonCross", align: "center", cellClick: (e, cell) => {
+                $("#ports-table").tabulator("deleteRow", cell.getRow());
+            }, width: 30, headerSort: false}
         ],
-        ajaxURL:"/rule",
+        ajaxURL: "/rule",
         ajaxResponse: (url, params, response) => {
             return response.data;
+        },
+        rowDeleted: (row) => {
+            alert("deleted row");
         }
     });
 });
