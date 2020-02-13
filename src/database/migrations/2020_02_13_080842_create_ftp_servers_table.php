b6e37ddbd0fb606c3a8d78b2d8a515437ae53287
mans1995 <49342726+mans1995@users.noreply.github.com>
2020-02-13 11:27:14 +0100

    fix: bad ftp server migration class name

--- src/database/migrations/2020_02_13_080842_create_ftp_servers_table.php
+++ src/database/migrations/2020_02_13_080842_create_ftp_servers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFileServersTable extends Migration
+class CreateFtpServersTable extends Migration
 {
     /**
      * Run the migrations.
