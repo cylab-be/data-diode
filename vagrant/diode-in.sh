3544143b2a52e48000a172676a24add03936d919
mans1995 <49342726+mans1995@users.noreply.github.com>
2020-02-26 10:07:16 +0100

    fix: changing command order in .sh files

--- vagrant/diode-in.sh
+++ vagrant/diode-in.sh
@@ -30,10 +30,10 @@ php artisan key:generate
 php artisan migrate
 php artisan config:reset
 cp -r /vagrant/BlindFTP_0.37 ..
+php artisan bftp:restart
+php artisan queue:checkup # launch the queue for the ftp server job -> before the chown because it creates /var/www/data-diode/src/storage/app/queue.pid
 chown -R www-data:www-data . ../BlindFTP_0.37
 sed -i -e "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 echo "www-data ALL=NOPASSWD: /var/www/data-diode/src/app/Scripts/datadiode.sh, /var/www/data-diode/BlindFTP_0.37, /bin/kill, /var/www/data-diode/src/artisan" | EDITOR="tee -a" visudo
 systemctl restart apache2
-php artisan bftp:restart
-php artisan queue:checkup # launch the queue for the ftp server job
