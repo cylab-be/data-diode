0647ace2356a250881899215fc15bfdd428455b2
mans1995 <49342726+mans1995@users.noreply.github.com>
2020-02-26 09:55:41 +0100

    fix: adding artisan to executable programs for www-data

--- vagrant/diode-in.sh
+++ vagrant/diode-in.sh
@@ -33,7 +33,7 @@ cp -r /vagrant/BlindFTP_0.37 ..
 chown -R www-data:www-data . ../BlindFTP_0.37
 sed -i -e "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
-echo "www-data ALL=NOPASSWD: /var/www/data-diode/src/app/Scripts/datadiode.sh, /var/www/data-diode/BlindFTP_0.37, /bin/kill" | EDITOR="tee -a" visudo
+echo "www-data ALL=NOPASSWD: /var/www/data-diode/src/app/Scripts/datadiode.sh, /var/www/data-diode/BlindFTP_0.37, /bin/kill, /var/www/data-diode/src/artisan" | EDITOR="tee -a" visudo
 systemctl restart apache2
 php artisan bftp:restart
 php artisan queue:checkup # launch the queue for the ftp server job
